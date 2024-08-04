3adacee09c8e71b42b967501ba97747273ebdfda
Ryan Matos <ryanferreiradematos@gmail.com>
2024-08-04 05:34:04 -0300

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# PackageDX
+
+PackageDX _(formerly **Package Description**)_ is a ready-to-use extension for providing packages' information.
+
+## Features
+
+Brings any package information by hovering it in `package.json`
+
+![alt text](./src/assets/imgs/image.png)
+
+## Release Notes
+
+### 1.0.0
+
+- Initial release of visualizing `description` and `found version` of hovered package;
+
---
+++ src/extension.ts
@@ -0,0 +1,61 @@
+import * as vscode from 'vscode';
+import { Hover, MarkdownString } from 'vscode';
+
+let PKGS_CACHE: Record<string, string> | undefined = {};
+
+export function activate(context: vscode.ExtensionContext) {
+   context.subscriptions.push(
+      vscode.languages.registerHoverProvider('json', { provideHover })
+   );
+}
+
+export function deactivate() {
+   PKGS_CACHE = undefined;
+}
+
+const provideHover: (
+   document: vscode.TextDocument,
+   position: vscode.Position,
+   token: vscode.CancellationToken
+) => vscode.ProviderResult<Hover> = (document, position) => {
+   const hoveredLine = document.lineAt(position.line);
+   const isPackage = new RegExp(
+      `"(?<key>dependencies|devDependencies)"\\s*:\\s*{[^}]*${hoveredLine.text
+         .trim()
+         .replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1')}[^}]*}`,
+      's'
+   ).exec(document.getText());
+
+   if (!isPackage) {
+      return null;
+   }
+
+   const pkg = hoveredLine.text.match(
+      /^\s+"((@[a-z0-9-~][a-z0-9-._~]*\/)?[a-z0-9-~][a-z0-9-._~]*)"/
+   )?.[1] as keyof typeof PKGS_CACHE;
+
+   if (PKGS_CACHE?.[pkg]) {
+      return new Hover(PKGS_CACHE?.[pkg]);
+   }
+
+   return new Promise(resolve => {
+      vscode.workspace.fs
+         .readFile(
+            vscode.Uri.joinPath(
+               vscode.workspace.workspaceFolders?.[0].uri!,
+               `node_modules/${pkg}/package.json`
+            )
+         )
+         .then(data => {
+            const msg = message(decode(data));
+            if (PKGS_CACHE) {
+               PKGS_CACHE[pkg] = msg;
+            }
+            resolve(new Hover(new MarkdownString(msg)));
+         });
+   });
+};
+
+const message = (json: any) =>
+   `${json.description}\n\n_Found version: \`v${json.version}\`_`;
+const decode = (data: any) => JSON.parse(new TextDecoder().decode(data));
