56b30ccb9d88235fbdc5562344ea7aa8fc0e7fcf
Ryan Matos <ryanferreiradematos@gmail.com>
2024-08-04 07:33:51 -0300

    feat: adding extension icon; and more
    - early return outside package or package-lock.json;
    - adding package not found message;
    - changing LICENSE.txt to .md;
    - changing extension displayName;
    - adding extension uthor;
    - adding extension galleryBanner;

--- src/extension.ts
+++ src/extension.ts
@@ -18,6 +18,8 @@ const provideHover: (
    position: vscode.Position,
    token: vscode.CancellationToken
 ) => vscode.ProviderResult<Hover> = (document, position) => {
+   if (vscode.window.activeTextEditor && !(new RegExp('^.*package(-lock)?.json$').test(vscode.window.activeTextEditor.document.fileName))) { return null; }
+   
    const hoveredLine = document.lineAt(position.line);
    const isPackage = new RegExp(
       `"(?<key>dependencies|devDependencies)"\\s*:\\s*{[^}]*${hoveredLine.text
@@ -33,6 +35,8 @@ const provideHover: (
    const pkg = hoveredLine.text.match(
       /^\s+"((@[a-z0-9-~][a-z0-9-._~]*\/)?[a-z0-9-~][a-z0-9-._~]*)"/
    )?.[1] as keyof typeof PKGS_CACHE;
+
+   if (!pkg) { return null; }
 
    if (PKGS_CACHE?.[pkg]) {
       return new Hover(PKGS_CACHE?.[pkg]);
@@ -52,10 +56,11 @@ const provideHover: (
                PKGS_CACHE[pkg] = msg;
             }
             resolve(new Hover(new MarkdownString(msg)));
-         });
+         }, () => resolve(new Hover(new MarkdownString(notFoundMessage(pkg)))));
    });
 };
 
 const message = (json: any) =>
    `${json.description}\n\n_Found version: \`v${json.version}\`_`;
+const notFoundMessage = (pkg: string) => `_Package \`${pkg}\` not found_`;
 const decode = (data: any) => JSON.parse(new TextDecoder().decode(data));
